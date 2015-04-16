56d45bc64763b3df5081002f1b971ba4f26cface
denodaeus <bobby.smith@bmarko.com>
2015-04-16 14:12:52 -0400

    postsLists route now takes a limit

--- server/publications.js
+++ server/publications.js
@@ -1,5 +1,9 @@
 Meteor.publish('posts', function() {
-  return Posts.find();
+  check(options, {
+    sort: Object,
+    limit: Number
+  });
+  return Posts.find({}, options);
 });
 
 Meteor.publish('comments', function(postId) {
